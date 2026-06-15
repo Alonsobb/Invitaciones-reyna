de6d03d2db89b4e39674ac8608537c7f2210fbca
Alonso Bravo Bernal <alonsobb_01@hotmail.com>
2026-06-15 10:20:02 -0600

    update

--- update.sh
+++ update.sh
@@ -5,6 +5,8 @@
 REPO_PATH="/mnt/Respaldos/data/respaldos/invitacion-reyna"
 
 echo "==> Descargando cambios de GitHub..."
+cd "$REPO_PATH" && git restore .
+cd "$REPO_PATH" && git reset --hard HEAD
+cd "$REPO_PATH" && git clean -fd
 cd "$REPO_PATH" && git pull origin main
-
 echo "✓ Actualización completada. Los cambios ya están activos en el sitio."
